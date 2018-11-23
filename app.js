a5ed88bf500a8e79b5383c5bd647724b1df03c99
harsha20599 <harsha20599@gmail.com>
2018-11-23 14:08:38 +0530

    Added redirect to https

--- app.js
+++ app.js
@@ -3,11 +3,13 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-
+var redirectToHTTPS = require('express-http-to-https').redirectToHTTPS;
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var app = express();
 
+app.use(redirectToHTTPS([/localhost:(\d{4})/], [/\/insecure/], 301));
+ 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
